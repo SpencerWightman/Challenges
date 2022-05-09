320635b73c3f09bfd4d82eb71bdc17ecd3c8e154
Spencer Wightman <spencer.wightman@gmail.com>
2022-05-08 23:20:44 -0400

    update

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,59 +1,3 @@
-
-=begin
-# -------------------------------------- Problem ----------------------------
-Goal
-  Convert modern number to its Roman numeral
-
-Rules
-  Express each digit separately starting with the left-most digit and skipping any     digit with a value of zero
-  
-  No need to be able to convert numbers larger than about 3000
-
-# -------------------------------------- Understand -------------------------
-Model
-  1990=MCMXC
-    1000=M
-    900=CM
-    90=XC
-  
-  2008=MMVIII
-    2000=MM
-    8=VIII
-    
-  22=XXII
-    20=XX
-    2=II
-    
-I=1  
-V=5  
-X=10  
-L=50  
-C=100  
-D=500  
-M=1000
-
-Abstract
-  Break given num into array of nums, then transform each num by adding as many zeros to it as there are nums left in the given num (next if num is 0)
-  
-  Create a constant hash with k/v of number to roman numeral
-  
-  Transform each element in this new array based on corresponding value in constant
-  
-  Join the array into a string
-
-# -------------------------------------- Slice ------------------------------
-
-
-
-# -------------------------------------- Code -------------------------------
-=end
-
-# one digit at a time to numeral
-# transform to 90
-# if 90 / __ > 0 then do it
-# put into new array of all nums
-# each of those nums turned into numeral, then joined
-
 def transform_to_nums
   num = 2398
   arr_num = num.digits.reverse
@@ -90,18 +34,3 @@ def transform_to_numerals(arr)
 end
 
 p transform_to_numerals(transform_to_nums) #== 'MMCCCXCVIII'
-
-=begin
-
-2398
-  2000 => MM
-  300 => CCC
-  90 => XC
-  8 => VIII
-=end
-
-
-
-
-
-
--- triangles.rb
+++ triangles.rb
@@ -1,33 +1,3 @@
-
-=begin
-# -------------------------------------- Problem ----------------------------
-Explicit
-  Return: equilateral; isosceles; scalene
-
-Rules
-  Equilateral: 3 / 3 / 3
-  Isosceles: 3 / 1 / 3
-  Scalene: 1 / 2 / 3
-  Sum of any 2 sides must be greater than any 1 side *
-  No side can have a length of 0 *
-
-# -------------------------------------- Understand -------------------------
-Collect sums of all 2 pairs of nums. Determine if they are 1) all the same 2) there is a duplicate 3) all unique. 
-Then, also that each is greater than each num in given array.
-
-# -------------------------------------- Slice ------------------------------
-(SIMPLIED FIRST STEP)
-Given array of 3 nums
-Iterate through array and return new array of sums of each unique pairing
-  Nested iteration
-    Next if indexes are the same
-    Add each element to the other element
-
-Return desired result based on the size of the sum array
-
-# -------------------------------------- Code -------------------------------
-=end
-
 class Triangle
   attr_reader :arr
 
